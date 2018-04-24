48e4e5691f7f046f8d5f691331bb69d5a4b831a6
Dylan <nielsen.dylan@gmail.com>
2018-04-24 10:52:41 -0700

    Add filtering, pagination, and aggregation

---
+++ src/resolvers/Feed.js
@@ -0,0 +1,8 @@
+'use strict';
+function links(parent, args, context, info) {
+  return context.db.query.links({ where: { id_in: parent.linkIds } }, info);
+}
+
+module.exports = {
+  links
+};
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,34 @@
 'use strict';
-function feed(root, args, context, info) {
-  return context.db.query.links({}, info);
+async function feed(root, args, context, info) {
+  const where = args.filter
+    ? {
+        OR: [
+          { url_contains: args.filter },
+          { description_contains: args.filter }
+        ]
+      }
+    : {};
+
+  const queriedLinks = await context.db.query.links(
+    { where, skip: args.skip, first: args.first, orderBy: args.orderBy },
+    '{ id }'
+  );
+
+  const countSelectionSet = `{
+      aggregate {
+        count
+      }
+    }`;
+
+  const linksConnection = await context.db.query.linksConnection(
+    {},
+    countSelectionSet
+  );
+
+  return {
+    count: linksConnection.aggregate.count,
+    linksId: queriedLinks.map(link => link.id)
+  };
 }
 
 function link(root, args, context, info) {
@@ -15,5 +43,6 @@ function link(root, args, context, info) {
 }
 
 module.exports = {
-  link, feed
+  link,
+  feed
 };
